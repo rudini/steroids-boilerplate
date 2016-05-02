f5f0ea182cc84aa78b6f9ead909bd36275fa06a4
Roger Rudin <roger.rudin@gmail.com>
2016-05-02 21:57:06 +0200

    unused code removed

--- app/example/scripts/SettingsController.ts
+++ app/example/scripts/SettingsController.ts
@@ -1,11 +1,4 @@
-// angular
-//   .module('example')
-//   .controller('SettingsController', function($scope, supersonic) {
-//     $scope.navbarTitle = "Settings";
-//   });
-
 export class SettingsController {
-  
   /* @ngInject */
   constructor($scope: Navbar, supersonic: supersonic) {
     $scope.navbarTitle = "Settings and more..."
