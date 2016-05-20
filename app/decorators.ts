5f19b8fb42634324f9a1a4cf0ac323e1a8eb219a
Roger Rudin <roger.rudin@hotmail.com>
2016-05-20 15:41:07 +0000

    Updated decorators.ts

--- app/decorators.ts
+++ app/decorators.ts
@@ -1,5 +1,5 @@
 export const Component = function(options: ng.IComponentOptions): Function {
-  return (controller: Function) => {
-    return angular.extend(options, {controller});
-  };
-};
+  return (controller: Function) => {    
+    return _.assign(options, {controller}, selector: _.camelCase(options.selector))
+  }
+}
